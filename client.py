659c35d7bb600f2f485289b3f15150198a86fcda
KennethMebara <mebaratsanga@et.esiea.fr>
2022-03-23 14:27:45 +0100

    modification

--- client.py
+++ client.py
@@ -19,14 +19,20 @@ class MainWindow(QWidget):
 
     def initUI(self):
         self.setWindowTitle("Client")
-        self.setFixedSize(400, 400)
-        self.label1 = QLabel("Enter your IP:", self)
+        self.setFixedSize(400,400)
+        self.label1 = QLabel("Host ip", self)
         self.text = QLineEdit(self)
         self.text.move(10, 30)
-        self.label2 = QLabel("Answer:", self)
-        self.label2.move(10, 60)
-        self.button = QPushButton("Send", self)
-        self.button.move(10, 90)
+        self.label2 = QLabel("API_key:", self)
+        self.label2.move(10,60)
+        self.text2 = QLineEdit(self)
+        self.text2.move(10,80)
+        self.label3 = QLabel("IP", self)
+        self.label3.move(10,100)
+        self.text3 = QLineEdit(self)
+        self.text3.move(10,130)
+        self.button = QPushButton("send", self)
+        self.button.move(10,150)
 
         self.button.clicked.connect(self.on_click)
         self.button.pressed.connect(self.on_click)
@@ -35,6 +41,8 @@ class MainWindow(QWidget):
 
     def on_click(self):
         hostname = self.text.text()
+        api_key =self.text.text()
+        ip = self.text.text()
 
         if hostname == "":
             QMessageBox.about(self, "Error", "Please fill the field")
@@ -42,6 +50,24 @@ class MainWindow(QWidget):
             res = self.__query(hostname)
             if res:
                 self.label2.setText("Answer%s" % (res["Hello"]))
+                self.label2.adjustSize()
+                self.show()
+        
+        if api_key == "":
+            QMessageBox.about(self, "Error", "Please fill the field")
+        else:
+            res1 = self.__query(api_key)
+            if res1:
+                self.label2.setText("Answer%s" % (res1["Hello"]))
+                self.label2.adjustSize()
+                self.show()
+
+        if ip == "":
+            QMessageBox.about(self, "Error", "Please fill the field")
+        else:
+            res2 = self.__query(ip)
+            if res2:
+                self.label2.setText("Answer%s" % (res2["Hello"]))
                 self.label2.adjustSize()
                 self.show()
 
